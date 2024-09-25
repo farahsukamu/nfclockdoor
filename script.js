d5b0c7adc3b214c95b85b67e94e57bce64010d55
farahsukamu <142573814+farahsukamu@users.noreply.github.com>
2024-09-25 20:10:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Konfigurasi Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyBU6FpDXlpesoWvfRc4ZtuM30tmW-yfiMQ",
-    authDomain: "rfid-lockdoor.firebaseapp.com",
-    databaseURL: "https://rfid-lockdoor-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "rfid-lockdoor",
-    storageBucket: "rfid-lockdoor.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",  // Dapatkan dari Firebase Console
-    appId: "YOUR_APP_ID"  // Dapatkan dari Firebase Console
+  apiKey: "AIzaSyBU6FpDXlpesoWvfRc4ZtuM30tmW-yfiMQ",
+  authDomain: "rfid-lockdoor.firebaseapp.com",
+  databaseURL: "https://rfid-lockdoor-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "rfid-lockdoor",
+  storageBucket: "rfid-lockdoor.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 // Inisialisasi Firebase
@@ -17,36 +17,33 @@ const database = firebase.database();
 
 // Fungsi untuk menambahkan UID baru
 function addUID() {
-    const newUID = document.getElementById('new-uid').value;
-    if (newUID) {
-        database.ref('UIDs/' + newUID).set({
-            uid: newUID
-        });
-        alert('UID berhasil ditambahkan!');
-    } else {
-        alert('Mohon masukkan UID.');
-    }
+  const newUID = document.getElementById('new-uid').value;
+  if (newUID) {
+    database.ref('UIDs/' + newUID).set({
+      uid: newUID
+    });
+    alert('UID berhasil ditambahkan!');
+  } else {
+    alert('Mohon masukkan UID.');
+  }
 }
 
 // Menampilkan UID yang terdaftar di list
 database.ref('UIDs').on('value', (snapshot) => {
-    const uidList = document.getElementById('uid-list');
-    uidList.innerHTML = '';
-    snapshot.forEach((childSnapshot) => {
-        const childData = childSnapshot.val();
-        const li = document.createElement('li');
-        li.textContent = childData.uid;
-        uidList.appendChild(li);
-    });
+  const uidList = document.getElementById('uid-list');
+  uidList.innerHTML = '';
+  snapshot.forEach((childSnapshot) => {
+    const childData = childSnapshot.val();
+    const li = document.createElement('li');
+    li.textContent = childData.uid;
+    uidList.appendChild(li);
+  });
 });
 
-// Fungsi untuk mengontrol servo
-function controlServo(action) {
-    database.ref('control/servo').set(action)
-        .then(() => {
-            alert('Perintah berhasil dikirim: ' + action);
-        })
-        .catch((error) => {
-            alert('Gagal mengirim perintah: ' + error);
-        });
+// Fungsi untuk menggerakkan servo
+function moveServo(angle) {
+  database.ref('Servo/').set({
+    position: angle
+  });
+  alert('Servo bergerak ke ' + angle + ' derajat!');
 }
